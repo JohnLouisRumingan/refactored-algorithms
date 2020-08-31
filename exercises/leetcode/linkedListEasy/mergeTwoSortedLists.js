99e25884e50650397e15cbf59b61a968609d88bc
John Louis Rumingan <jmrumingan@gmail.com>
2020-08-31 13:42:09 -0400

    Change mergeTwoSortedLists

--- exercises/leetcode/linkedListEasy/mergeTwoSortedLists.js
+++ exercises/leetcode/linkedListEasy/mergeTwoSortedLists.js
@@ -24,4 +24,28 @@ Output: 1->1->2->3->4->4
  */
 var mergeTwoLists = function(l1, l2) {
     
-};
+    //     dummy node
+        var l3 = new ListNode(null, null);
+        var prev = l3;
+        
+        while(l1 !== null && l2 !== null){
+            if(l1.val < l2.val){
+                prev.next = l1;
+                l1 = l1.next;
+            }
+    //         previously had this as an else if with separate = statement but it doesn't matter due to ordered list
+            else{
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            
+            prev = prev.next;
+        }
+        
+    //     once one list is null, we know that because the list is sorted, we can append the other list
+        if(l1 === null){prev.next = l2;}
+        if(l2 === null){prev.next = l1;}
+        
+    //     return next so as to not include the dummy node
+        return l3.next; 
+    };

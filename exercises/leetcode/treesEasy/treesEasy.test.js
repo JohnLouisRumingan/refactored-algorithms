173fa5f693451de51e3d2fed43a7b8d6b3a344d4
John Louis Rumingan <jmrumingan@gmail.com>
2020-09-05 15:55:27 -0400

    Add sortedArrayToBST function

--- exercises/leetcode/treesEasy/treesEasy.test.js
+++ exercises/leetcode/treesEasy/treesEasy.test.js
@@ -1,6 +1,7 @@
 const maxDepth = require("./maxDepthOfBinaryTree")
 const isValidBST = require("./validateBinarySearchTree")
 const levelOrder = require("./binaryTreeLevelOrderTraversal")
+const importSortedArrayToBST = require("./convertSortedArrayToBST")
 
 function TreeNode(val, left, right) {
     this.val = (val===undefined ? 0 : val)
@@ -41,4 +42,20 @@ test('validateBST of binary tree with values [2, 1, 3] should return true', () =
 
 test("levelOrder with values [3,9,20,null,null,15,7] should return [[3],[9,20],[15,7]]", () =>{
     expect(levelOrder(treeRoot)).toStrictEqual([[3],[9,20],[15,7]])
+})
+
+test("when importSortedArrayToBST is given [-10,-3,0,5,9] it should return [0,-3,9,-10,null,5]", () => {
+    
+    let treeRoot = new TreeNode(0);
+    let treeNeg3 = new TreeNode(-3);
+    let treeNeg10 = new TreeNode(-10);
+    let tree9 = new TreeNode(9);
+    let tree5 = new TreeNode(5);
+
+    treeRoot.left = treeNeg3;
+    treeRoot.right = tree9;
+    treeNeg3.left = treeNeg10;
+    tree9.left = tree5;
+
+    expect(importSortedArrayToBST([-10, -3, 0, 5, 9])).toStrictEqual()
 })

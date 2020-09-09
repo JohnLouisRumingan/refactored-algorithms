915e71efb8cb06ab989eb5683e9c64839d1659f4
John Louis Rumingan <jmrumingan@gmail.com>
2020-09-09 08:48:45 -0400

    Change maxStairs to use memoization

--- exercises/leetcode/easyInterview/dynamicProgrammingEasy/climbStairs.js
+++ exercises/leetcode/easyInterview/dynamicProgrammingEasy/climbStairs.js
@@ -30,7 +30,19 @@ Constraints:
 
 function maxStairs(n){
 
-    let permutations = 0;
+    let memo = [];
 
-    return permutations;
+    return climbStairs(0, n, memo)
+}
+
+
+function climbStairs(i, n, memo){
+
+    if(i>n) return 0;
+    if(i === n) return 1;
+    if(memo[i] > 0 ){
+        return memo[i];
+    }
+    memo[i] = climbStairs(i+1, n, memo) + climbStairs(i+2, n, memo);
+    return memo;
 }

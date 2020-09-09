e0a54c23aeb65bbfde597559d6314a55bbea70f1
John Louis Rumingan <jmrumingan@gmail.com>
2020-09-09 12:59:47 -0400

    Add romanToInteger

---
+++ exercises/leetcode/easyInterview/designEasy/shuffleDeck.js
@@ -0,0 +1,61 @@
+/*
+  Shuffle an Array
+
+Shuffle a set of numbers without duplicates.
+
+Example:
+
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();
+*/
+
+/**
+ * @param {number[]} nums
+ */
+var Solution = function(nums) {
+    
+    this.deck = nums;
+};
+
+/**
+ * Resets the array to its original configuration and return it.
+ * @return {number[]}
+ */
+Solution.prototype.reset = function() {
+    
+    return this.deck;
+};
+
+/**
+ * Returns a random shuffling of the array.
+ * @return {number[]}
+ */
+Solution.prototype.shuffle = function() {
+    
+    let a = [...this.deck];
+    
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
+
+ module.exports = Solution;

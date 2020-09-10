cda2b0c4bf39da50ea93b1666a4df10ed9676fbb
John Louis Rumingan <jmrumingan@gmail.com>
2020-09-10 13:03:10 -0400

    Add bullsAndCows with test

--- exercises/leetcode/september20Challenge.js/september20.test.js
+++ exercises/leetcode/september20Challenge.js/september20.test.js
@@ -2,6 +2,7 @@ const containsNearbyAlmostDuplicate = require("./containsDuplicate")
 const wordPattern = require("./wordPattern")
 const sumRootToLeaf = require("./sumRootToLeafBinary")
 const compareVersion = require("./compareVersionNumbers")
+const getHint = require("./bullsAndCows");
 
 function TreeNode(val, left, right) {
     this.val = (val===undefined ? 0 : val)
@@ -61,4 +62,8 @@ test('when compareVersion is given 1.01 and 1.001 it returns 0', () =>{
 
 test('when compareVersion is given 1.0 and 1.0.0 it returns 0', () =>{
     expect(compareVersion("1.0", "1.0.0")).toBe(0);
+})
+
+test('when getHint is given ""43800797115925613013", "60371817651755202260", it returns "2A12B"', () => {
+    expect(getHint("43800797115925613013", "60371817651755202260")).toBe("2A12B")
 })

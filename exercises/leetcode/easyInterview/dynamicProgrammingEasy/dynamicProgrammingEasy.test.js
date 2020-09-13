a4ca6e835355696c4a4a226bd530659d596fa10a
John Louis Rumingan <jmrumingan@gmail.com>
2020-09-13 12:07:55 -0400

    Add houseRobber and tests

--- exercises/leetcode/easyInterview/dynamicProgrammingEasy/dynamicProgrammingEasy.test.js
+++ exercises/leetcode/easyInterview/dynamicProgrammingEasy/dynamicProgrammingEasy.test.js
@@ -1,6 +1,7 @@
 const maxProfit = require('./bestTimeToBuyAndSellStock');
 const maxStairs = require('./climbStairs');
 const maxSubarray = require('./maximumSubarray');
+const rob = require('./houseRobber');
 
 test('when maxProfit is given [7,1,5,3,6,4] it should return 5', () => {
     expect(maxProfit([7,1,5,3,6,4])).toBe(5);
@@ -27,7 +28,21 @@ test('maxSubarray correctly returns the output of an array containing a single e
     expect(maxSubarray([1])).toBe(1);
     expect(maxSubarray([0])).toBe(0);
     expect(maxSubarray([-2147483647])).toBe(-2147483647);
+})
 
+test('houseRobber returns the correct maximum sum from provided test cases', () => {
+    expect(rob([1,2,3,1])).toBe(4);
+    expect(rob([2,7,9,3,1])).toBe(12);
+})
+
+test('houseRobber gives correct return for edge cases', () => {
+    expect(rob([])).toBe(0);
+    expect(rob()).toBe(0);
+})
+
+test('houseRobber does not compare two subarrays starting at i=0 and i=1', () => {
+    expect(rob([2,1,1,2])).toBe(4);
+    expect(rob([2,1,1,2])).not.toBe(3);
 })
 
 // test('when maxStairs is given an input of 2 it should return 2', () => {

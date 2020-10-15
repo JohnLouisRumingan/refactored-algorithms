560fdc94b607b3ab94a9dfd4d846e004d3fe289a
John Louis Rumingan <55402017+JohnLouisRumingan@users.noreply.github.com>
2020-10-15 10:57:32 -0400

    Add houseRobber2 algorithm

--- exercises/leetcode/october20Challenge.js/houseRobber2.js
+++ exercises/leetcode/october20Challenge.js/houseRobber2.js
@@ -31,3 +31,46 @@ Constraints:
     0 <= nums[i] <= 1000
 
 */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    
+    if(nums.length <=0 || nums == null) return 0;
+    if(nums.length == 1 ) return nums[0];
+    
+//     add all houses from element 0 to n-1
+//     add all houses from element 1 to n
+//     return higher value
+//     create two subArrays, one starting from i=0 until i=nums.length-2, other from i=1 to i=nums.length-1
+    let house0 = nums.slice(0, nums.length-1);
+    let house1 = nums.slice(1, nums.length);
+    
+    console.log(houseRob(house0), houseRob(house1))
+    
+    return Math.max(houseRob(house0), houseRob(house1))
+};
+
+function houseRob(nums){
+    
+    if(nums.length <=0 || nums == null) return 0;
+    if(nums.length == 1 ) return nums[0];
+    
+    let total = [];
+    total[0] = nums[0];
+    total[1] = (Math.max(nums[0], nums[1]));
+    
+    for(let i=2; i<nums.length; i++){
+        //create temp subarray, then push the max value to total array
+        let tempSub = total.slice(0, i-1);
+        tempSub = tempSub.map(num => num+nums[i]);
+        tempSub.push(nums[i]);
+        total.push(Math.max(...tempSub))
+    }
+    
+    return Math.max(...total);
+}
+
+module.exports = rob;
--- exercises/leetcode/october20Challenge.js/october20.test.js
+++ exercises/leetcode/october20Challenge.js/october20.test.js
@@ -1,5 +1,6 @@
 const { TestScheduler } = require('jest')
 const buddyStrings = require('./buddyStrings').buddyStrings
+const rob = require('./houseRobber2');
 
 test('when buddystrings is given A="ab" and B="ba" it returns true', () => {
     expect(buddyStrings("ab", "ba")).toBe(true);
@@ -7,4 +8,20 @@ test('when buddystrings is given A="ab" and B="ba" it returns true', () => {
     
 test('A=aa B=aa returns true', () => {
     expect(buddyStrings("aa", "aa")).toBe(true);
+})
+
+test('when houseRobber is given [2,3,2] it returns 3', () => {
+    expect(rob([2,3,2])).toBe(3);
+})
+
+test('when houseRobber is given [1,2,3,1] it returns 4', () => {
+    expect(rob([1,2,3,1])).toBe(4);
+})
+
+test('when houseRobber is given [0] it returns 0', () => {
+    expect(rob([0])).toBe(0);
+})
+
+test('when houseRobber is given [1] it returns 1', () => {
+    expect(rob([1])).toBe(1);
 })
